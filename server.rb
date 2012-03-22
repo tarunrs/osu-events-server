3a13006143062d7083edd01150f974bf9f182e4d
Tarun R S <tarunrs@gmail.com>
2012-03-22 18:52:33 -0400

    get_events API added. takes num_events and last_event_id as parameters. return a list of events. Defaults to 50 events

--- server.rb
+++ server.rb
@@ -2,10 +2,9 @@ require 'rubygems'
 require 'open-uri'
 require 'sinatra'
 require 'json'
-require 'hpricot'
 require 'cgi'
 require 'active_record'
-#require 'iconv'
+
 ActiveRecord::Base.establish_connection(
  :adapter => "mysql",
  :host => "localhost",
@@ -20,17 +19,12 @@ class Users < ActiveRecord::Base
 end
 
 get '/' do
+    headers({"Content-Type" => "text/html; charset=ISO-8859-1"})
     "OSU Events"
-end
-
-get '/test' do
-	headers({"Content-Type" => "text/html; charset=ISO-8859-1"})
-	erb("hello")
 end
 
 get '/new_user' do
     res = Users.create()
-    puts res.user_id
     headers({"Content-Type" => "text/html; charset=ISO-8859-1"})
     {"result" => {"status" => true, "data" => res.user_id }}.to_json
 end
@@ -45,6 +39,56 @@ get '/connect_user' do
         return {"result" => {"status" => false, "data" => {"user_id" => user.user_id, "fb_user_id" => user.fb_id, "fb_access_token" => user.fb_access_token, "user_name" => user.user_name} }}.to_json
     else
         return {"result" => {"status" => true, "data" => {"user_id" => user.user_id, "fb_user_id" => user.fb_id, "fb_access_token" => user.fb_access_token, "user_name" => user.user_name} }}.to_json
+    end
+end
+
+get '/get_event_details' do
+    event_id = params[:event_id]
+    puts event_id
+    event_details = Event_Details.find(event_id)
+    if (event_details.nil?)
+        return {"result" => {"status" => false, "data" => nil }}.to_json
+    else
+        return {"result" => {"status" => true, "data" => {
+            "event_id" => event_details.event_id,
+            "name" => event_details.name,
+            "start_date" => event_details.start_date,
+            "end_date" => event_details.end_date,
+            "contact_email"=> event_details.contact_email,
+            "contact_name" => event_details.contact_name,
+            "contact_number" => event_details.contact_number,
+            "category" => event_details.category,
+            "event_type" => event_details.event_type,
+            "event_link" => event_details.event_link,
+            "details_link" => event_details.details_link,
+            "location" => event_details.location,
+            "description" => event_details.description }}}.to_json
+    end
+end
+
+get '/get_events' do
+    num_events = params[:num_events]
+    last_event_id = params[:last_event_id]
+    if (num_events.nil?) 
+        num_events = 50
+    end
+    if (last_event_id.nil?)
+        events = Event_Details.find(:all,:select => ["event_id", "name", "location", "start_date", "end_date", "category"], 
+                                    :limit => num_events, :order => "event_id", :conditions => ["start_date >= ?", Date.today])
+        if (events.nil?)
+            return {"result" => {"status" => false, "data" => nil }}.to_json
+        else
+            return {"result" => {"status" => true, "data" => events }}.to_json
+        end
+    else
+        events = Event_Details.find(:all, :select => ["event_id", "name", "location", "start_date", "end_date", "category"], 
+                                    :limit => num_events, :order => "event_id", :conditions => ["start_date >= ? AND event_id > ?", Date.today, last_event_id])
+        if (events.nil?)
+            return {"result" => {"status" => false, "data" => nil }}.to_json
+        else
+            return {"result" => {"status" => true, "data" => events }}.to_json
+        end
+
     end
 end
 
